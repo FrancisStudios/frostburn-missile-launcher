6dcb673620a05ccfa3d62322420e465fbe89e542
unknown <vlfmap@gmail.com>
2023-08-11 08:04:42 +0200

    added more error handling

--- missile-launcher-bot/main.js
+++ missile-launcher-bot/main.js
@@ -99,7 +99,21 @@ const haltFrostburn = () => {
             method: 'POST',
             body: '{ "command": "stop" }',
             headers: { 'Content-Type': 'application/json' }
-        });
+        }).then(
+            function(response) {
+              if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' +
+                  response.status);
+                return;
+              }
+              response.json().then(function(data) {
+                console.log(data);
+              });
+            }
+          )
+          .catch(function(err) {
+            console.log('Fetch Error :-S', err);
+          });
     } catch (error) {
         console.log('No response API server...')
     }
